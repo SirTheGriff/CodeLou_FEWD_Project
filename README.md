549c1cc23a042cfde0c2d9549356495cc7950c40
Justin <sirthegriff@yahoo.com>
2018-02-24 10:09:59 -0500

    first commit

--- README.md
+++ README.md
@@ -2,30 +2,18 @@
 
 ## Description
 ```
-A brief description of your project
-
-```
+This is a game for my daughter, who loves matching games. When looking at elementary schools for her, the guides/teachers indicated that children don't know how to use a mouse because of the prevalence of touch screens. I created a matching game so that she can learn how to use a mouse. With images, I can change the game to different themes like family pictures, animals, or cartoons. I added a number to the times clicked as a reference of how quickly the user moves through the game. The game tracks the matched items until the end so that an alert pops up indicating the game is finished. From clicking the bubbles, the reset button, and the alert, my daughter should get plenty of practice using a mouse.
 
 
 
 ## Custom CSS Classes
 ```
-The class(es) I created are:
-
-1. class name ex( .main-info-section )
-.. what class does, ex( adds padding & changes background color of .main-info-section )
-
-2.
+All CSS was written by me outside the gradients and shadows. I used uiGradients.com for the background and header background. I used a few shadow generators for the bubbles, however tweaked them after appying them to the page.
 
 ```
-
-
 
 ## Custom JavaScript Functions
 ```
-The javascript functions I created are:
-
-1. function name ex( animate() )
-.. what function does ex( animate() is used to move the elements in the info div across the screen)
+I used 3 to 4 different sources for some of the JavaScript, either for big ideas or small aspects of the code that I couldn't get to work on my own. However the majority was done by me, trial and error. I left most of the trial and error code at the bottom, commented out, to show the progess and ideas that didn't work.
 
 ```
---
+++ js/app.js
@@ -0,0 +1,306 @@
+var firstClick = "";
+var secondClick = "";
+var score = 0;
+var prevTarget = null;
+var delay = 2000;
+var count = 0;
+var matchEnd = 0;
+var pictures = [{
+      'name': 'amadablam',
+      'img': 'img/amadablam.jpg',
+    },
+    {
+      'name': 'bridge',
+      'img': 'img/bridge.jpg',
+    },
+    {
+      'name': 'equipment',
+      'img': 'img/equipment.jpg',
+    },
+    {
+      'name': 'guitar',
+      'img': 'img/guitar.jpg',
+    },
+    {
+      'name': 'plouzane',
+      'img': 'img/plouzane.jpg',
+    },
+    {
+      'name': 'speaker',
+      'img': 'img/speaker.jpg',
+    },
+    {
+      'name': 'stonehenge',
+      'img': 'img/stonehenge.jpg',
+    },
+    {
+      'name': 'subaru',
+      'img': 'img/subaru.jpg',
+    },
+    {
+      'name': 'castle',
+      'img': 'img/castle.jpg',
+    },
+    {
+      'name': 'dumbells',
+      'img': 'img/dumbells.jpg',
+    },
+    {
+      'name': 'runners',
+      'img': 'img/runners.jpg',
+    },
+    {
+      'name': 'puppy',
+      'img': 'img/puppy.jpg',
+    },
+    {
+      'name': 'jeep',
+      'img': 'img/jeep.jpg',
+    },
+    {
+      'name': 'dog',
+      'img': 'img/dog.jpg',
+    },
+    {
+      'name': 'door',
+      'img': 'img/door.jpg',
+    },
+    {
+      'name': 'money',
+      'img': 'img/money.jpg',
+    },
+    {
+      'name': 'amadablam',
+      'img': 'img/amadablam.jpg',
+    },
+    {
+      'name': 'bridge',
+      'img': 'img/bridge.jpg',
+    },
+    {
+      'name': 'equipment',
+      'img': 'img/equipment.jpg',
+    },
+    {
+      'name': 'guitar',
+      'img': 'img/guitar.jpg',
+    },
+    {
+      'name': 'plouzane',
+      'img': 'img/plouzane.jpg',
+    },
+    {
+      'name': 'speaker',
+      'img': 'img/speaker.jpg',
+    },
+    {
+      'name': 'stonehenge',
+      'img': 'img/stonehenge.jpg',
+    },
+    {
+      'name': 'subaru',
+      'img': 'img/subaru.jpg',
+    },
+    {
+      'name': 'castle',
+      'img': 'img/castle.jpg',
+    },
+    {
+      'name': 'dumbells',
+      'img': 'img/dumbells.jpg',
+    },
+    {
+      'name': 'runners',
+      'img': 'img/runners.jpg',
+    },
+    {
+      'name': 'puppy',
+      'img': 'img/puppy.jpg',
+    },
+    {
+      'name': 'jeep',
+      'img': 'img/jeep.jpg',
+    },
+    {
+      'name': 'dog',
+      'img': 'img/dog.jpg',
+    },
+    {
+      'name': 'door',
+      'img': 'img/door.jpg',
+    },
+    {
+      'name': 'money',
+      'img': 'img/money.jpg',
+    }
+];
+
+pictures.sort(function(a, b){return 0.5 - Math.random()});
+
+var container = document.getElementById("container");
+
+pictures.forEach(item => {
+  const gameCard = document.createElement('div');
+  const gameImage = document.createElement('span');
+  gameCard.classList.add('gameimage');
+  gameCard.dataset.name = item.name;
+  gameCard.style.backgroundImage = `url(${item.img})`;
+  gameImage.classList.add('gamesquare');
+  container.appendChild(gameCard);
+  gameCard.appendChild(gameImage);
+});
+
+
+container.addEventListener('click', function(event){
+    var clicked = event.target;
+      if (clicked.nodeName === 'DIV' || clicked === prevTarget || clicked.parentNode.classList.contains('selected')
+            ) {
+        return;
+      }
+      if (count < 2) {
+        count ++;
+        if (count === 1) {
+        firstClick = clicked.parentNode.dataset.name;
+        clicked.parentNode.classList.add('selected');
+        clicked.classList.remove('gamesquare');
+        score += 1;
+          } else {
+        secondClick = clicked.parentNode.dataset.name;
+        clicked.parentNode.classList.add('selected');
+        clicked.classList.remove('gamesquare');
+        score += 1;
+        matchEnd += 2;
+        }
+        if (firstClick && secondClick) {
+          if (firstClick === secondClick) {
+            setTimeout(matchedCards, delay, removeSpan);
+            setTimeout(resetMatch, delay);
+          }
+            setTimeout(resetUnmatch, delay);
+       }
+       prevTarget = clicked;
+     }
+     var scoreClicks = document.getElementById("clicks").innerHTML = score;
+});
+
+
+
+function matchedCards() {
+  var matched = document.querySelectorAll(".selected");
+  matched.forEach(square => {
+  square.classList.add('matchedItem');
+  square.classList.remove('.selected');
+});;
+}
+
+function removeSpan() {
+var removeSpan = document.querySelectorAll(".matchedItem");
+removeSpan.forEach(span => {
+  span.firstChild.remove(".gamesquare");
+})
+}
+
+const resetMatch = () => {
+  firstClick = "";
+  secondClick = "";
+  count = 0;
+  var matched= document.querySelectorAll(".selected")
+  matched.forEach(square => {
+    square.classList.remove('selected');
+  });
+  alertEndGame();
+};
+
+const resetUnmatch = () => {
+  firstClick = "";
+  secondClick = "";
+  count = 0;
+  var unmatched = document.querySelectorAll(".selected")
+  unmatched.forEach(square => {
+    square.classList.remove('selected');
+    square.firstChild.classList.add('gamesquare')
+  });
+};
+
+function alertEndGame() {
+const endGame = document.getElementsByClassName("matchedItem");
+if (matchEnd === 32) {
+  alert("FANTASTIC! YOU DID IT! Click OK and then RESET to play again.");
+};
+}
+
+var button = document.getElementById("resetButton");
+button.addEventListener('click', function(){
+  location.reload();
+});
+
+
+
+//// Old code below
+
+// var cardsNode = document.querySelectorAll(".gameimage")
+// var cardsPlayArray = [];
+// for (var i = 0; i < cardsNode.length; i++) {
+//   cardsPlayArray.push(cardsNode[i]);
+//
+//
+// window.onload = gameStart();
+//
+//
+// // makes an array from the generation of the squares which is nodeList
+//
+// var nodeList = document.querySelectorAll('.gamesquare');
+// var squareArray = [];
+// for (var i = 0; i < nodeList.length; i++) {
+//   squareArray.push(nodeList[i]);
+// }
+//
+// //this should be the game part
+// for (var i = 0; i < squareArray.length; i++) {
+// squareArray[i].addEventListener('click', function(){
+//     this.classList.add("gameimage");
+//     this.style.backgroundSize = "100px 100px";
+//     this.style.backgroundImage = pictures[j]
+//     });
+// }
+
+
+// square.classList.remove("gamesquare");
+//   square.addEventListener("click", function() {
+
+
+// square.classList.add("gameimage");
+// squares.appendChild(square);
+// square.style.backgroundImage = pictures[i];
+// square.style.backgroundSize = "100px 100px";
+
+
+// function generateImage () {
+//  for (var i = 0; i <= pictures.length; i ++) {
+// image.setAttribute("src", randomPictures[i]);
+//  }
+// }
+
+// var showPictures = function clicked() {
+//   this.classList.toggle("open");
+//   this.classList.toggle("close");
+//   this.classList.toggle("disabled");
+// }
+
+
+// function gamePlay () {
+//   for (var i = 0; i < pictures.length; i++) {
+//
+//     }
+//   };
+//
+//  gamePlay();
+
+// for (var i = 0; i <= pictures.length; i ++) {
+//   var gameCard = document.createElement('div');
+//   gameCard.setAttribute('class', 'gameimage');
+//   container.appendChild(gameCard);
+//   gameCard.dataset.name = pictures[i].name;
+//   gameCard.style.backgroundImage = `url(${item.img})`;
+//   container.appendChild(gameCard);
+// };
